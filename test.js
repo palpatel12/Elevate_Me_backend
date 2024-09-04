55ddee2930098b4b5e6fec85ef0ec3931baa519e
palpatel12 <pal12052003@gmail.com>
2024-09-04 19:09:33 +0530

    commit

---
+++ test.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+const Building = require('./models/Building');
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
+
+const testBuilding = new Building({
+    hostelType: 'Men',
+    blockName: 'A Block',
+    lifts: []
+});
+
+testBuilding.save()
+    .then(building => console.log(building))
+    .catch(err => console.log(err))
+    .finally(() => mongoose.connection.close());
